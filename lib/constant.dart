1f1e719fbf592c0c81e9ecbd78e29544372ceadd
mohamedkhaled <mohammedkhaled156212gmail.com>
2023-10-15 19:10:27 +0300

    initial commet

---
+++ lib/constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff3799fb);
+const kSecondaryColor = Color(0xff2b374f);
+const kDarkColor = Color(0xff000714);
+const kBodyTextColor = Color(0xff979dac);
+const kBgColor = Color(0xff001233);
+
+const kDefaultPadding = 20.0;
